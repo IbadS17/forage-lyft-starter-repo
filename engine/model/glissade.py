27ccb052ca50565eba92747e5ff93e85866b2076
unknown <ibads66585@gmail.com>
2024-09-07 14:18:11 +0530

    Initial Commit

--- engine/model/glissade.py
+++ engine/model/glissade.py
@@ -1,12 +1,9 @@
-from datetime import datetime
+from car import Car
 
-from engine.willoughby_engine import WilloughbyEngine
+class Glissade(Car):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
 
-
-class Glissade(WilloughbyEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return self.engine.needs_service() or self.battery.needs_service()
